c24f9962e0412424a9ce147d649cf8bc1ea54a80
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-02-06 22:39:18 -0800

    updated first layout of page

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  //   document.getElementById("mySidenav").style.width = "50%";
+  document.getElementById("mySidenav").style.transform = "translate(-100%,0)";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  //   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.transform = "translate(100%,0)";
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      "libre-franklin": ["Libre Franklin", "Helvetica", "Arial", "sans-serif"],
+      archivo: ["Archivo", "Helvetica", "Arial", "sans-serif"],
+      "sorts-mill": ["Sorts Mill Goudy", "serif"],
+      bellefair: ["Bellefair", "serif"],
+    },
   },
   plugins: [],
 };
