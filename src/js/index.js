fafdcdca1efb5be72af7af6ddeef481a1b6f35cf
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-02-10 01:59:20 -0800

    added dark mode with session storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,36 @@
+let checkbox = document.getElementById("dark-mode"); //get the checkbox to a variable
+
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("darkmode") == true) {
+  darkmode(); //if dark mode was on, run this funtion
+} else {
+  nodark(); //else run this funtion
+}
+
+//if the checkbox state is changed, run a funtion
+checkbox.addEventListener("click", function () {
+  //check if the checkbox is checked or not
+  if (!checkbox.checked) {
+    darkmode(); //if the checkbox is checked, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
+});
+
+//function for checkbox when checkbox is checked
+function darkmode() {
+  document.documentElement.classList.add("dark"); //add a class to the body tag
+  checkbox.checked = true; //set checkbox to be checked state
+  sessionStorage.setItem("darkmode", true); //store a name & value to know that dark mode is on
+}
+
+//function for checkbox when checkbox is not checked
+function nodark() {
+  document.documentElement.classList.remove("dark"); //remove added class from body tag
+  checkbox.checked = false; //set checkbox to be unchecked state
+  sessionStorage.setItem("darkmode", false); //store a name & value to know that dark mode is off or light mode is on
+}
+
 function reveal() {
   let reveals = document.querySelectorAll(".reveal");
 
